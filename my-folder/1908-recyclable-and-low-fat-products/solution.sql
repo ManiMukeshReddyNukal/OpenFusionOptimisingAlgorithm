3fb67d0557134caf1d6f7faf736fda0da494a176
ManiMukeshReddyNukal <114439598+ManiMukeshReddyNukal@users.noreply.github.com>
2024-02-12 20:50:31 +0000

    [LeetCode Sync] Runtime - 1297 ms (19.80%), Memory - 0B (100.00%)

--- my-folder/1908-recyclable-and-low-fat-products/solution.sql
+++ my-folder/1908-recyclable-and-low-fat-products/solution.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 select product_id
-from Products 
-where low_fats = 'Y' AND recyclable = 'Y';
+from Products
+where low_fats = "Y" AND recyclable = "Y" ;
 
 
